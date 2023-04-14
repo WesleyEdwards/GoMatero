e7ad084761c19223c8c19e21c970b6e0e15e1bf6
Joshua McClung <josh.mcclung66@gmail.com>
2023-04-13 19:28:50 -0600

    Map centers in on you

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,37 @@
-import GoogleMapReact from 'google-map-react';
+import {GoogleMap, useJsApiLoader, useGoogleMap} from '@react-google-maps/api';
+import { useCallback, useEffect, useState } from 'react';
 export const Map = () => {
 
+    const { isLoaded } = useJsApiLoader({
+        id: 'goMatero',
+        googleMapsApiKey: import.meta.env.VITE_MAPS_API_KEY
+    })
 
-    return (
-        <div style ={{ height: '100vh', width: '100%'}}>
-            <GoogleMapReact
-            bootstrapURLKeys={{key:""}}>
-            </GoogleMapReact>
-        </div>
+    return ( isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={{width:'80vw',height:'80vh'}}
+            center = {{lat:0,lng:0}}
+            zoom={10}
+            onLoad={map => {
+                const bounds = new window.google.maps.LatLngBounds();
+                map.fitBounds(bounds);
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        (position: GeolocationPosition) => {
+                            const pos = {
+                                lat: position.coords.latitude,
+                                lng: position.coords.longitude,
+                            }; 
+                            map.panTo(pos);
+                        }
+                    )
+                }
+            }}
+            onUnmount={map => {
+                // Do nothing for now
+            }}></GoogleMap> ) : <></>
     )
 }
+
+
+export default Map;
