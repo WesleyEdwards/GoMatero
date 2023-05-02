cfaa4a6debd81d2caf87502265a1df72c1c3840d
WesleyEdwards <westheedwards@gmail.com>
2023-05-02 16:30:11 -0600

    formatting files

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,52 +1,64 @@
-import {GoogleMap, useJsApiLoader, useGoogleMap, OverlayView} from '@react-google-maps/api';
-import { useCallback, useContext, useEffect, useState } from 'react';
-import Pin from './Pin';
-import { MateSession } from '../utils/models';
-import { AuthContext } from '../context/AuthContext';
+import { GoogleMap, useJsApiLoader, OverlayView } from "@react-google-maps/api";
+import { useContext } from "react";
+import Pin from "./Pin";
+import { AuthContext } from "../context/AuthContext";
+import { Spinner } from "./Spinner";
 export const Map = () => {
+  const { myAttended: sessions } = useContext(AuthContext);
 
-    const { myAttended: sessions } = useContext(AuthContext);
+  const { isLoaded } = useJsApiLoader({
+    id: "goMatero",
+    googleMapsApiKey: import.meta.env.VITE_MAPS_API_KEY,
+  });
 
-    const { isLoaded } = useJsApiLoader({
-        id: 'goMatero',
-        googleMapsApiKey: import.meta.env.VITE_MAPS_API_KEY
-    })
+  if (!isLoaded) return <Spinner />;
 
-    return ( isLoaded ? (
-        <>
-        <GoogleMap
-            mapContainerStyle={{width:'98vw',height:'90vh'}}
-            center = {{lat:41.749837,lng:-111.832160}}
-            zoom={10}
-            onLoad={map => {
-                const bounds = new window.google.maps.LatLngBounds();
-                map.fitBounds(bounds);
-                if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(
-                        (position: GeolocationPosition) => {
-                            const pos = {
-                                lat: position.coords.latitude,
-                                lng: position.coords.longitude,
-                            }; 
-                            map.panTo(pos);
-                        },
-                        (err:GeolocationPositionError) => {
-                            if (err.code == err.PERMISSION_DENIED) {
-                                alert("Please Enable location to automatically pan the map to your position.");
-                            }
-                        }
-                        )
-                    }
-                }}
-            onUnmount={map => {
-                // Do nothing for now
-            }}>{sessions.map((session)=>{
-                return <OverlayView key={session.id} position={session.location} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}><Pin key={session.id} session={session} position={session.location}/></OverlayView>
-            })}</GoogleMap>
-            </> ) : <></>
-            
-    )
-}
-
+  return (
+    <GoogleMap
+      mapContainerStyle={{ width: "98vw", height: "90vh" }}
+      center={{ lat: 41.749837, lng: -111.83216 }}
+      zoom={10}
+      onLoad={(map) => {
+        const bounds = new window.google.maps.LatLngBounds();
+        map.fitBounds(bounds);
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position: GeolocationPosition) => {
+              const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              };
+              map.panTo(pos);
+            },
+            (err: GeolocationPositionError) => {
+              if (err.code == err.PERMISSION_DENIED) {
+                alert(
+                  "Please Enable location to automatically pan the map to your position."
+                );
+              }
+            }
+          );
+        }
+      }}
+      onUnmount={(map) => {}}
+    >
+      {sessions.map((session) => {
+        return (
+          <OverlayView
+            key={session.id}
+            position={session.location}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+          >
+            <Pin
+              key={session.id}
+              session={session}
+              position={session.location}
+            />
+          </OverlayView>
+        );
+      })}
+    </GoogleMap>
+  );
+};
 
 export default Map;
--- src/components/Pin.tsx
+++ src/components/Pin.tsx
@@ -3,53 +3,55 @@ import { AuthContext } from "../context/AuthContext";
 import { LatLng, MateSession } from "../utils/models";
 
 type PinProps = {
-    session:MateSession,
-    position:LatLng
-}
+  session: MateSession;
+  position: LatLng;
+};
 
-export const Pin = ({session, position}:PinProps) => {
-    const [isFocused, setIsFocused] = useState(false);
-    const { api } = useContext(AuthContext);
-    const pinRef = useRef<HTMLDivElement>(null);
-    const [members,setMembers] = useState("");
+export const Pin = ({ session, position }: PinProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const { api } = useContext(AuthContext);
+  const pinRef = useRef<HTMLDivElement>(null);
+  const [memberNames, setMemberNames] = useState("");
 
-    useEffect(() => {
-        api.publicUsers(session.attendedMembers)
-        .then(users => {
-            var res = "";
-            users.forEach(user => {
-                res += user.name + " ";
-            });
-            setMembers(res)
-        })
-    },[])
+  useEffect(() => {
+    api.publicUsers(session.attendedMembers).then((users) => {
+      const names = users.map((user) => user.name);
+      setMemberNames(names.join(", "));
+    });
+  }, []);
 
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (pinRef.current && !pinRef.current.contains(event.target as Node)) {
-                setIsFocused(false);
-            }
-        }
-        document.addEventListener('click', handleClickOutside);
-        return ()=> {
-            document.removeEventListener('click', handleClickOutside);
-        };
-    }, [pinRef]);
-    return (
-        <div className={isFocused ? 'PinFocused':'PinUnfocused'} ref={pinRef} onClick={()=>setIsFocused(true)}>
-            <div>
-            <img src={session.image} alt={session.title}/>
-            {isFocused?
-            <>
-                <h4>{session.title}</h4>
-                <p>{session.description}</p>
-                <p>{session.date}</p>
-                <p>{members}</p>
-            </>
-                :<></>}
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (pinRef.current && !pinRef.current.contains(event.target as Node)) {
+        setIsFocused(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [pinRef]);
+  return (
+    <div
+      className={isFocused ? "PinFocused" : "PinUnfocused"}
+      ref={pinRef}
+      onClick={() => setIsFocused(true)}
+    >
+      <div>
+        <img src={session.image} alt={session.title} />
+        {isFocused ? (
+          <>
+            <h4>{session.title}</h4>
+            <p>{session.description}</p>
+            <p>{session.date}</p>
+            <p>{memberNames}</p>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Pin;
